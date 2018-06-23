982fb055a17597547f807001268dcbf1f0f18908
Chris Chung <christopher0chung@gmail.com>
2018-06-23 16:54:31 -0500

    Cam update in test scene_Chris.

---
+++ LookingGlass2018_v1.0/Assets/Scripts/SimpleFog.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SimpleFog : MonoBehaviour
+{
+    [Range(0.001f, 1)]
+    public float cutoff = 0.5f;
+    Material mat;
+
+    void OnEnable()
+    {
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+        mat = new Material(Shader.Find("HoloPlay/Simple Fog"));
+        UpdateMaterial();
+    }
+
+    void OnDisable()
+    {
+        DestroyImmediate(mat);
+    }
+
+    void Update()
+    {
+        UpdateMaterial();
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, mat);
+    }
+
+    void UpdateMaterial()
+    {
+        if (mat != null)
+        {
+            mat.SetFloat("cutoff", cutoff);
+        }
+    }
+}
