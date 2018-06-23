2e226b92938b5c36abc840c84ee35631604e09c2
Chris Chung <christopher0chung@gmail.com>
2018-06-23 12:50:40 -0500

    Rotation script and pivot prefab.

---
+++ LookingGlass2018_v1.0/Assets/RotationController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationController : MonoBehaviour {
+
+    public HoloPlay.Capture myCap;
+
+    public float rotAngle { get; private set; }
+    public float rotSpeedInput;
+    public float rotSpeedOutput;
+
+    private Vector3 _rotToSet;
+
+	void Start () {
+        _rotToSet = Vector3.zero;
+	}
+	
+	void Update () {
+        InputMethod();
+        SetRotationAngle();
+	}
+
+    private void InputMethod ()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            rotAngle += rotSpeedInput;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            rotAngle -= rotSpeedInput;
+        }
+
+        rotAngle = Mathf.Clamp(rotAngle, 0, 360);
+    }
+
+    private void SetRotationAngle()
+    {
+        _rotToSet.y = rotAngle;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(_rotToSet), rotSpeedOutput);
+    }
+}
