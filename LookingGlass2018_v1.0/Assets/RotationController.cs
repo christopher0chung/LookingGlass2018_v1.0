415e9a30583eeedb395960f1bf6a7758052dbcbf
seth s scott <sether19@gmail.com>
2018-06-23 15:22:39 -0400

    Auto stash before merge of "master" and "origin/master"
    Added objects turning on and off based on camera rotation

--- LookingGlass2018_v1.0/Assets/RotationController.cs
+++ LookingGlass2018_v1.0/Assets/RotationController.cs
@@ -13,14 +13,53 @@ public class RotationController : MonoBehaviour {
     private Vector3 _rotToSet;
     private float _rotRateCalculated;
 
+    public GameObject[] aObjs;
+    public GameObject[] bObjs;
+
 	void Start () {
         _rotToSet = Vector3.zero;
+
+        aObjs = GameObject.FindGameObjectsWithTag("A");
+        bObjs = GameObject.FindGameObjectsWithTag("B");
+
 	}
-	
-	void Update () {
+
+    void Update()
+    {
         InputMethod();
         SetRotationAngle();
-	}
+        print(transform.rotation);
+
+
+        ///Turning on and off A Objects
+        if (transform.rotation.y <= 0.7f) { 
+            foreach (GameObject a in aObjs)
+            {
+                a.SetActive(true);
+            }
+        } else {
+            foreach (GameObject a in aObjs)
+            {
+                a.SetActive(false);
+            }
+        }
+
+        ///Turning on and off B Objects
+        if (transform.rotation.y >= 0.7f)
+        {
+            foreach (GameObject b in bObjs)
+            {
+                b.SetActive(true);
+            }
+        } else {
+            foreach (GameObject b in bObjs)
+            {
+                b.SetActive(false);
+            }
+        }
+
+    }
+
 
     private void InputMethod ()
     {
@@ -41,5 +80,6 @@ public class RotationController : MonoBehaviour {
         _rotToSet.y = rotAngle;
 
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(_rotToSet), rotSpeedOutput);
+     
     }
 }
