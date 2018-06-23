830c74e80649f293e615c4e7c8bd616a7fde2114
Chris Chung <christopher0chung@gmail.com>
2018-06-23 15:01:26 -0500

    Test scene - Chris updated.

--- LookingGlass2018_v1.0/Assets/RotationController.cs
+++ LookingGlass2018_v1.0/Assets/RotationController.cs
@@ -10,6 +10,14 @@ public class RotationController : MonoBehaviour {
     public float rotSpeedInput;
     public float rotSpeedOutput;
 
+    [Header("Tuning Variables")]
+    public float sizeA;
+    public float sizeB;
+    public float sizeC;
+    public float fovA;
+    public float fovB;
+    public float fovC;
+
     private Vector3 _rotToSet;
     private float _rotRateCalculated;
 
@@ -20,6 +28,7 @@ public class RotationController : MonoBehaviour {
 	void Update () {
         InputMethod();
         SetRotationAngle();
+        AToBCamXfm();
 	}
 
     private void InputMethod ()
@@ -41,5 +50,19 @@ public class RotationController : MonoBehaviour {
         _rotToSet.y = rotAngle;
 
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(_rotToSet), rotSpeedOutput);
+    }
+
+    private void AToBCamXfm()
+    {
+        if (rotAngle < 180)
+        {
+            myCap.Size = Mathf.Lerp(sizeA, sizeB, rotAngle / 180);
+            myCap.Fov = Mathf.Lerp(fovA, fovB, rotAngle / 180);
+        }
+        else
+        {
+            myCap.Size = Mathf.Lerp(sizeB, sizeC, (rotAngle - 180) / 180);
+            myCap.Fov = Mathf.Lerp(fovB, fovC, (rotAngle - 180) / 180);
+        }
     }
 }
