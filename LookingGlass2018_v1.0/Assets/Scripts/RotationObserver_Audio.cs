267fd314bf1f6ca1844c0d1256cbb060ac7b9da7
Chris Chung <christopher0chung@gmail.com>
2018-06-24 13:19:29 -0500

    Audio rotation observer.

---
+++ LookingGlass2018_v1.0/Assets/Scripts/RotationObserver_Audio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationObserver_Audio : MonoBehaviour {
+
+    private AudioSource _AS1;
+    private AudioSource _AS2;
+
+    [SerializeField] private RotationController _rotationController;
+
+    public AudioClip ac1;
+    public AudioClip ac2;
+
+    // Use this for initialization
+    void Start()
+    {
+        _AS1 = gameObject.AddComponent<AudioSource>();
+        _AS2 = gameObject.AddComponent<AudioSource>();
+
+        Debug.Assert(_rotationController != null, "RotationController reference is missing");
+
+        _AS1.clip = ac1;
+        _AS2.clip = ac2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _AS1.volume = Mathf.Lerp(1, 0, _rotationController.rotAngle / 360);
+        _AS2.volume = Mathf.Lerp(0, 1, _rotationController.rotAngle / 360);
+    }
+}
