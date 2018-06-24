fa26febf27de34163b7df8807d46d43cbd35165f
Chris Chung <christopher0chung@gmail.com>
2018-06-24 18:37:04 -0500

    BGM and FastForward.

---
+++ LookingGlass2018_v1.0/Assets/BGM.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour {
+
+    public AudioClip myAC;
+
+	// Use this for initialization
+	void Start () {
+		if (GameObjects.Find("BGM").Count > 1)
+        {
+            Destroy(this.gameObject);
+
+            AudioSource myAS = gameObject.AddComponent<AudioSource>();
+            myAS.loop = true;
+            myAS.clip = myAC;
+            myAC.Play();
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+	}
+}
---
+++ LookingGlass2018_v1.0/Assets/FastForward.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FastForward : MonoBehaviour {
+
+    TextMesh myTM;
+
+    float timer;
+
+    int displayedTime;
+
+    void Start () {
+        myTM = GetComponent<TextMesh>();
+        timer = 15.99f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        displayedTime = (int)timer;
+
+        myTM.text = displayedTime.ToString();
+
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+	}
+}
