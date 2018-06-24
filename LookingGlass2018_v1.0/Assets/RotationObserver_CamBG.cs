d9f3bb57162a97f3ae9fb182f3d3b25c0ef91e65
Chris Chung <christopher0chung@gmail.com>
2018-06-24 18:10:17 -0500

    Fonts and edits.

---
+++ LookingGlass2018_v1.0/Assets/RotationObserver_CamBG.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationObserver_CamBG : MonoBehaviour {
+
+    public Color c1;
+    public Color c2;
+
+    public RotationController myRC;
+
+    void Start()
+    {
+        Debug.Assert(myRC != null, "Missing Rotation Controller reference");
+    }
+
+	void Update () {
+        HoloPlay.Capture.Instance.cam.backgroundColor = Color.Lerp(c1, c2, myRC.rotAngle / 360);
+    }
+}
